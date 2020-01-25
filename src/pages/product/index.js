e239d14edac609e05ecdfa89514e9f97a4d65b92
Lucas Nogueira <lucas_snogueira@hotmail.com>
2020-01-25 02:18:39 -0200

    adiciona pagina de detalhe do produto

--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -1,9 +1,30 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import api from "../../services/api";
+
+import './style.css'
 
 export default class Product extends Component {
+  state = {
+    product: {}
+  };
+
+  async componentDidMount() {
+    const { id } = this.props.match.params;
+
+    const promise = api.get(`/products/${id}`);
+
+    this.setState({ product: (await promise).data });
+  }
+
   render() {
+    const { product } = this.state;
+
     return (
-      <h1>podruto</h1>
+      <div className="product-info">
+        <strong>{product.title}</strong>
+        <p>{product.description}</p>
+        <a href={product.url}>{product.url}</a>
+      </div>
     );
   }
 }
