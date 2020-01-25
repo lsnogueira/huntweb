5a4f7836f946912554906d18eddbccd1df3b56e0
Lucas Nogueira <lucas_snogueira@hotmail.com>
2020-01-25 00:23:31 -0200

    adiciona estilo da aplicacao

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import api from "../../services/api";
 
+import './styles.css';
+
 export default class Main extends Component {
   state = {
     products: []
@@ -17,10 +19,16 @@ export default class Main extends Component {
   };
 
   render() {
+    const { products } = this.state;
+
     return (
       <div className="product-list">
-        {this.state.products.map(item => (
-          <h2 key={item._id}>{item.title}</h2>
+        {products.map(product => (
+          <article key={product._id}>
+            <strong>{product.title}</strong>
+            <p>{product.description}</p>
+            <a href="">Acessar</a>
+          </article>
         ))}
       </div>
     );
